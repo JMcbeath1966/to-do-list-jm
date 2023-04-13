38cce6ddc172c23ec660225593501650606315e2
James McBeath <jamesmcbeath@me.com>
2023-04-13 15:25:10 +0000

    Update routes.py, categories and edit_category html

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,7 +5,7 @@ from taskmanager.models import Category, Task
 
 # These are calling python functions, not the app route
 @app.route("/")
-def home():  
+def home():                           
     return render_template("tasks.html")
 
 
@@ -25,3 +25,11 @@ def add_category():
     return render_template("add_category.html")
 
 
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
